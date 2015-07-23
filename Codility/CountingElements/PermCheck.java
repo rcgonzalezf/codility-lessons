65f6893219fa7b759b4ccc6068d2821f334b21d8
Roberto Carlos Gonzalez Flores <iscrobertogonzalez@gmail.com>
2015-07-23 06:57:07 -0700

    Adding solution to PermCheck

--- Codility/CountingElements/PermCheck.java
+++ Codility/CountingElements/PermCheck.java
@@ -1,9 +1,16 @@
 /**
-*
+* https://codility.com/demo/results/demoYEU94K-8FU/ 100
 */
 public class PermCheck {
+
+  public static final int NOT_PERMUTATION = 0;
+  public static final int PERMUTATION = 1;
+
   // (4,1,3,2) = 1
   // (4,1,3) = 0
+  // (1) = 1
+  // () = 1
+  // (2) = 0
   public int solution(int[] A) {
     // write your code in Java SE 8
     int[] mark = new int[A.length + 1];
@@ -23,5 +30,5 @@ public class PermCheck {
     }
 
     return counter == A.length ? PERMUTATION : NOT_PERMUTATION;
-}
+  }
 }
