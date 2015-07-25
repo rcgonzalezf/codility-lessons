ffd53797b6ceec6bb47b857e6cbcbd1db294dfd0
Roberto Carlos Gonzalez Flores <iscrobertogonzalez@gmail.com>
2015-07-24 23:50:52 -0700

    Missing Integer problem with 100

--- Codility/CountingElements/MissingInteger.java
+++ Codility/CountingElements/MissingInteger.java
@@ -1,10 +1,14 @@
+import java.util.HashSet;
+
 // Submission
 // https://codility.com/demo/results/demoS8Y67D-HHQ/ 77
 public class MissingInteger {
 
+  // shuffled sequence 1, 2, ..., 100000 (without minus)
+  // got 1 expected 1000001
   // (-2,1) = 1
   // (2) = 1
-  // (1) = 0
+  // (1) = 2
   // (1,2,4) = 3
   public int solution(int[] A) {
       // write your code in Java SE 8
