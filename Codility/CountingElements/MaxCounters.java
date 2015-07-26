b2584c4ce08eb0f71aaa9f789303adf2dc4f650b
Roberto Carlos Gonzalez Flores <iscrobertogonzalez@gmail.com>
2015-07-25 19:27:28 -0700

    Updating the solution I have for 100%

--- Codility/CountingElements/MaxCounters.java
+++ Codility/CountingElements/MaxCounters.java
@@ -2,6 +2,7 @@
 // https://codility.com/demo/results/demoSEJHZS-ZPR/ 100
 public class MaxCounters {
 
+  // Some testcases
   // (1,[1,2,3]) = [1]
   // (1,[1]) = [1]
   // (1,[5]) = [0]
@@ -9,24 +10,31 @@ public class MaxCounters {
   // (2,[1,1,1,2,3,1]) = [4,3]
   // (5, [3, 4, 4, 5, 1, 4, 4]) = (1, 0, 1, 4, 1)
   public int[] solution(int N, int[] A) {
-          int length = A.length, maxOfCounter = 0;
-          int applyMax = N + 1;
-          int result[] = new int[N];
+      int length = A.length, maxOfCounter = 0, lastUpdate = 0;
+      int applyMax = N + 1;
+      int result[] = new int[N];
 
-          for (int i = 0; i < length; ++i ) {
-              if (A[i] <= N) {
-                  result[A[i]-1] = result[A[i]-1] + 1;
+      for (int i = 0; i < length; ++i ) {
+          if(A[i] == applyMax){
+              lastUpdate = maxOfCounter;
+          } else if (A[i] <= N)  {
+              int position = A[i]-1;
+              result[position] = result[position] > lastUpdate
+                                        ? result[position] + 1 : lastUpdate + 1;
 
-                  if(maxOfCounter <=  result[A[i]-1]) {
-                      maxOfCounter = result[A[i]-1];
-                  }
-              } else if(A[i] == applyMax){
-                  for (int j = 0; j < result.length; ++j) {
-                   result[j] = maxOfCounter;
-                  }
+              // updating the max for future use
+              if(maxOfCounter <=  result[position]) {
+                  maxOfCounter = result[position];
               }
           }
+     }
+     // updating all the values that are less than the lastUpdate to the max value
+     for (int i = 0; i < N; ++i) {
+         if(result[i] < lastUpdate) {
+             result[i] = lastUpdate;
+         }
+     }
 
-          return result;
-  }
+     return result;
+}
 }
