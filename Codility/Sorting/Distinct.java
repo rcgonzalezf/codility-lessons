0b0bf127e9c95d416f39d56943072b3327e21293
Roberto Carlos Gonzalez Flores <iscrobertogonzalez@gmail.com>
2015-08-08 14:20:32 -0700

    Adding a solution without set

--- Codility/Sorting/Distinct.java
+++ Codility/Sorting/Distinct.java
@@ -2,6 +2,7 @@ import java.util.Set;
 import java.util.HashSet;
 
 // https://codility.com/demo/results/demoVS545C-F36/  100%
+// https://codility.com/demo/results/demoX7MSFZ-JMB/ 100%
 public class Distinct {
 
     // testcases
@@ -21,4 +22,40 @@ public class Distinct {
 
         return distinctCount;
     }
+
+    public int solutionWithoutSet(int[] A) {
+        int length = A.length;
+        int inputLimit = 1000000;
+        int[] positives = new int[inputLimit];
+        int[] negatives = new int[inputLimit]; // should be length - 1 not counting zero
+
+        for (int element : A) {
+            if ( element >=0 ) {
+                ++positives[element];
+            } else {
+                int abs = element * -1;
+                ++negatives[abs];
+            }
+        }
+
+
+        int countDistincts = 0;
+
+        for (int i = 0; i < inputLimit; ++i) {
+            if ( positives[i] != 0 ) {
+                ++countDistincts;
+            }
+
+            if ( negatives[i] != 0 ) {
+                ++countDistincts;
+            }
+
+            if ( countDistincts == length ) {
+                break; // no need to compare all elements
+            }
+        }
+
+        return countDistincts;
+    }
+
 }
