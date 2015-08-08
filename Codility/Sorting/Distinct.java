4cd0fea7690327a01e2859a26785c34b295ce5bb
Roberto Carlos Gonzalez Flores <iscrobertogonzalez@gmail.com>
2015-08-08 14:43:46 -0700

    Another distinct solution
    This time I’m not iterating over the “large” arrays, only N elements.

--- Codility/Sorting/Distinct.java
+++ Codility/Sorting/Distinct.java
@@ -3,6 +3,7 @@ import java.util.HashSet;
 
 // https://codility.com/demo/results/demoVS545C-F36/  100%
 // https://codility.com/demo/results/demoX7MSFZ-JMB/ 100%
+// https://codility.com/demo/results/demoMTWUSD-S9M/ 100%
 public class Distinct {
 
     // testcases
@@ -57,5 +58,38 @@ public class Distinct {
 
         return countDistincts;
     }
+
+    public int solutionWithoutSetCountUntilInputLength(int[] A) {
+       int length = A.length;
+       int inputLimit = 1000000;
+       int[] positives = new int[inputLimit];
+       int[] negatives = new int[inputLimit]; // should be length - 1 not counting zero
+
+       for (int element : A) {
+           if ( element >=0 ) {
+               ++positives[element];
+           } else {
+               int abs = element * -1;
+               ++negatives[abs];
+           }
+       }
+
+       int countDistincts = 0;
+
+       for (int i: A) {
+           if (i >= 0 ) {
+               if ( positives[i] >= 1 ) {
+                   ++countDistincts;
+                   positives[i] = 0;
+               }
+           } else {
+               if ( negatives[i * -1] >= 1 ) {
+                   ++countDistincts;
+                   negatives[i * -1] = 0;
+               }
+           }
+       }
+       return countDistincts ;
+   }
 
 }
